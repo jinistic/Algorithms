b83d9f8e4d125ee3e5874473320cbee0921f78f0
jin <jinistic@gmail.com>
2021-09-23 18:35:36 +0900

    https://leetcode.com/problems/valid-palindrome/

---
+++ python algorithm interview/01 유효한 팰린드롬.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        word = []
+        for s_ in s:
+            if s_.isalnum():
+                word.append(s_.lower())
+        
+        len_word = len(word)
+        for i in range(len_word // 2):
+            if word[i] != word[len_word - 1 - i]:
+                return False
+            
+        return True
+    
+    
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
+print(sol.isPalindrome("race a car"))
+print(sol.isPalindrome("0p"))
