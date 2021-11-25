a799c3604930ef9f6f930c74b386afba003455c2
jin <jinistic@gmail.com>
2021-11-25 23:58:46 +0900

    https://leetcode.com/problems/product-of-array-except-self/

--- python algorithm interview/11 자신을 제외한 배열의 곱.py
+++ python algorithm interview/11 자신을 제외한 배열의 곱.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        product_from_left = [1]
-        product_from_right = [1]
+        product_from_left = []
+        product_from_right = []
         pl = 1
         pr = 1
-        for i in range(len(nums) - 1):
-            pl *= nums[i]
-            pr *= nums[-i - 1]
+        for i in range(len(nums)):
             product_from_left.append(pl)
             product_from_right.append(pr)
+            pl *= nums[i]
+            pr *= nums[-i - 1]
 
         return [
             product_from_left[i] * product_from_right[-i - 1] for i in range(len(nums))
