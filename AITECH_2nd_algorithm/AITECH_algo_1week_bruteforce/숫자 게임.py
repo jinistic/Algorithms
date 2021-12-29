f90c7c5939576919103baa2cec72965b4db4a9ec
jin <jinistic@gmail.com>
2021-12-29 10:10:21 +0900

    https://www.acmicpc.net/problem/2303

--- AITECH_2nd_algorithm/AITECH_algo_1week_bruteforce/숫자 게임.py
+++ AITECH_2nd_algorithm/AITECH_algo_1week_bruteforce/숫자 게임.py
@@ -5,20 +5,20 @@ read_input = sys.stdin.readline
 
 N = int(read_input())
 total_max_units = -float("inf")
-person_number_with_max_units = 0
+per_num_with_max_units = 0
 
-for person_number in range(1, N + 1):
+for per_num in range(1, N + 1):
     five_cards = list(map(int, read_input().split()))
-    personal_max_units = -float("inf")
+    per_max_units = -float("inf")
 
     for three_cards in combinations(five_cards, 3):
         units = sum(three_cards) % 10
 
-        if units >= personal_max_units:
-            personal_max_units = units
+        if units >= per_max_units:
+            per_max_units = units
 
-    if personal_max_units >= total_max_units:
-        total_max_units = personal_max_units
-        person_number_with_max_units = person_number
+    if per_max_units >= total_max_units:
+        total_max_units = per_max_units
+        per_num_with_max_units = per_num
 
-print(person_number_with_max_units)
+print(per_num_with_max_units)
